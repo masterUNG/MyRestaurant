cc5b30d43eb7eab1d179ad35818cddf00a1e07a9
masterUNG <phrombutr@gmail.com>
2015-02-10 16:20:41 +0700

    SQLite

--- app/src/main/java/appewtc/masterung/myrestaurant/MyOpenHelper.java
+++ app/src/main/java/appewtc/masterung/myrestaurant/MyOpenHelper.java
@@ -7,12 +7,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 /**
  * Created by masterUNG on 2/10/15 AD.
  */
-public class MyOpenHelper extends SQLiteOpenHelper{
+public class MyOpenHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "Restaurant.db";
     private static final int DATABASE_VERSION = 1;
     private static final String USER_TABLE = "create table userTABLE (_id integer primary key, " + " User text, Password text, Officer text);";
-    private static final String ORDER_TABLE = "create table orderTABLE (_id integer primary key, "+" Officer text, Date text, Food text, Item integer);";
+    private static final String ORDER_TABLE = "create table orderTABLE (_id integer primary key, " + " Officer text, Date text, Food text, Item integer);";
 
     public MyOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,7 +21,7 @@ public class MyOpenHelper extends SQLiteOpenHelper{
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(USER_TABLE);
-       db.execSQL(ORDER_TABLE);
+        db.execSQL(ORDER_TABLE);
     }
 
     @Override
